b412c1d6949c558ebd22b8ad8197cde933fa2c4c
Jonathon Green <ericg9811@gmail.com>
2019-07-15 15:49:28 -0500

    Stretch - Made seperate components for scoreboard and buttons

--- src/App.js
+++ src/App.js
@@ -1,75 +1,26 @@
-//TODO: STEP 1 - Import the useState hook.
-import React, {
-    useState
-} from "react";
+import React, { useState } from "react";
 import "./App.css";
-import BottomRow from "./BottomRow";
+import TopRow from "./TopRow.js";
+import BottomRow from "./BottomRow.js";
+import Buttons from "./Buttons.js";
 
 function App() {
-    //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-    const [lions, lionsScore] = useState(0);
-    const [tigers, tigersScore] = useState(0);
+    const [home, homeScore] = useState(0);
+    const [away, awayScore] = useState(0);
 
-
-    return ( <
-        div className = "container" >
-        <
-        section className = "scoreboard" >
-        <
-        div className = "topRow" >
-        <
-        div className = "home" >
-        <
-        h2 className = "home__name" > Lions < /h2>
-
-        {
-            /* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */
-        }
-
-        <
-        div className = "home__score" > {
-            lions
-        } < /div> < /
-        div > <
-        div className = "timer" > 00: 03 < /div> <
-        div className = "away" >
-        <
-        h2 className = "away__name" > Tigers < /h2> <
-        div className = "away__score" > {
-            tigers
-        } < /div> < /
-        div > <
-        /div> <
-        BottomRow / >
-        <
-        /section> <
-        section className = "buttons" >
-        <
-        div className = "homeButtons" > {
-            /* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */
-        } < button onClick = {
-            () => lionsScore(lions + 7)
-        }
-        className = "homeButtons__touchdown" > Home Touchdown < /button>   <
-        button onClick = {
-            () => lionsScore(lions + 3)
-        }
-        className = "homeButtons__fieldGoal" > Home Field Goal < /button> < /
-        div >
-        <
-        div className = "awayButtons" >
-        <
-        button onClick = {
-            () => tigersScore(tigers + 7)
-        }
-        className = "awayButtons__touchdown" > Away Touchdown < /button>   <
-        button onClick = {
-            () => tigersScore(tigers + 3)
-        }
-        className = "awayButtons__fieldGoal" > Away Field Goal < /button>  < /
-        div > <
-        /section>  < /
-        div >
+    return ( 
+        <div className = "container">
+            <section className = "scoreboard">
+                <TopRow home={home} away={away} />
+                <BottomRow />
+            </section>
+            <Buttons
+                home={home}
+                away={away}
+                homeScore={homeScore}
+                awayScore={awayScore}
+            />
+        </div>
     );
 }
 
